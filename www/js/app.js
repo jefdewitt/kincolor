23e335a43883a0447d1a7089a3ef01f640f5ee02
Jef DeWitt <jefdewitt@gmail.com>
2017-05-25 08:28:32 -0400

    adds checkbox

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@ angular.module('kinColor',[])
 
 .directive('colorInput', function() {
     return {
-        template: '<form><label name="color"><h1>Input Color</h1></label><input ng-model="color.value" ng-change="updateFoo(color.value)" name="color"></form><div><h1  >{{ greetMe }}</h1></div>',
+        template: '<form><label name="color"><h1>Input Color</h1></label><input ng-model="color.value" ng-change="updateFoo(color.value)" name="color"><label name="type">rgb<input type="checkbox" ng-click="test()" ng-model="type.value"><label></form><div><h1>{{ newColor }}</h1></div>',
         controller: 'colorCtrl',
         restrict: 'AE'
     };
@@ -11,57 +11,38 @@ angular.module('kinColor',[])
 
 .controller('colorCtrl', function($scope) {
 
-    $scope.greetMe = 'The input field is empty!';
+    $scope.newColor = 'The input field is empty!';
 
     $scope.updateFoo = function (newFoo) {
-      console.log(newFoo);
 
-        $scope.greetMe = newFoo;
+        // if ($scope.type.value) {
+        // } else {
+            var rgbColor = convertHex(newFoo);
+            $scope.newColor = rgbColor;
+        // }
     }
 
-    // var currentColor = $scope.color.value;
-    // if (currentColor) {
-    //   $scope.greetMe = currentColor;
-    // } else {
-    //   $scope.greetMe = 'no color input yet';
-    // }
+    $scope.test = function() {
+        console.log($scope.type.value);
+    }
 
-    // var convert = sharedObject;
-    // console.info(convert);
-    // convert.hex.lab('DEADBF');
+    function componentToHex(c) {
+      var hex = c.toString(16);
+      return hex.length == 1 ? "0" + hex : hex;
+    }
 
-    // var returnValue = $http.get() {
-    //   .then(function (response) {
-    //
-    //     $log.info(response);
-    //   })
-    // }
+    function rgbToHex(r, g, b) {
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
 
-    // function componentToHex(c) {
-    //   var hex = c.toString(16);
-    //   return hex.length == 1 ? "0" + hex : hex;
-    // }
-    //
-    // function rgbToHex(r, g, b) {
-    //     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-    // }
-    //
-    // function hexToRgb(hex) {
-    //   // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    //   var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    //   hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-    //       return r + r + g + g + b + b;
-    //   });
-    //
-    //   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    //   return result ? {
-    //       r: parseInt(result[1], 16),
-    //       g: parseInt(result[2], 16),
-    //       b: parseInt(result[3], 16)
-    //   } : null;
-    // }
-    //
-    // var rgbColor = hexToRgb(currentColor);
-    // $scope.greetMe = currentColor;
+    function convertHex(hex){
+        hex = hex.replace('#','');
+        r = parseInt(hex.substring(0,2), 16);
+        g = parseInt(hex.substring(2,4), 16);
+        b = parseInt(hex.substring(4,6), 16);
+        result = 'rgb('+r+','+g+','+b+')';
+        return result;
+    }
+
 
 });
